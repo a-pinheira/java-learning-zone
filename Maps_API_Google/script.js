6643355aacd89f03381d716769932f8e17a9123d
Mony Machado <monymachad@gmail.com>
2021-01-28 09:31:22 -0300

    Update Marcador instancia de objeto - JS

--- Maps_API_Google/script.js
+++ Maps_API_Google/script.js
@@ -8,5 +8,15 @@ function initMap() {
             lat: 40.7413549, lbg: -73.9980244},
             zoom: 13
     });
+    var tribe = {
+        lat: 40.719526, lng: -74.0089934
+    };
+    //instancia do objeto google.maps
+    var marker = new google.maps.Marker({
+        position: tribe,
+        map: map,
+        //título do mapa
+        title: "First Marker! - Primeira construção!"
+    });
   
 }
---
+++ eCommerce/src/main/java/com/packageOther/ProjetoTeste/ConfiguracaoExterna.java
@@ -0,0 +1,11 @@
+package com.packageOther.ProjetoTeste;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfiguracaoExterna {
+    //só para exemplificar tipos de configuração externa se, se fizer necessário
+}
+
+
+
