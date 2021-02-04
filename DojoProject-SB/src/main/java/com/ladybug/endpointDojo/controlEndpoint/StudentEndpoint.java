ab209a27baff2b6fc6fe0f93cc9ca7d80c4b0da5
Mony Machado <monymachad@gmail.com>
2021-02-03 21:54:15 -0300

    add Setup Project complete 1°- dojo

--- DojoProject-SB/src/main/java/com/ladybug/endpointDojo/controlEndpoint/StudentEndpoint.java
+++ DojoProject-SB/src/main/java/com/ladybug/endpointDojo/controlEndpoint/StudentEndpoint.java
@@ -1,4 +1,29 @@
 package com.ladybug.endpointDojo.controlEndpoint;
 
+import com.ladybug.endpointDojo.model.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import static java.util.Arrays.asList;
+
+/*
+@author Simone
+ */
+@RestController
+//dando um nome para o endpoint
+@RequestMapping("study")
+//ponto de acesso final (pessoas/sistemas) de onde os users acessarão a API
+//Endpoint
 public class StudentEndpoint {
+    //adc os metodos
+    //a forma como se retornar o valores deste metodo
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll(){
+        return asList(
+                new Student("Simone"),
+                new Student("Simonetti")
+        );
+    }
 }
--- DojoProject-SB/src/main/java/com/ladybug/endpointDojo/start/ApplicationStart.java
+++ DojoProject-SB/src/main/java/com/ladybug/endpointDojo/start/ApplicationStart.java
@@ -3,11 +3,12 @@ package com.ladybug.endpointDojo.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
-//Vai configurar a aplicação baseado nas dependecias que foram adc
 @EnableAutoConfiguration
+//Vai configurar a aplicação baseado nas dependecias que foram adc e o pacote de controler
+@ComponentScan(basePackages = "com.ladybug.endpointDojo.controlEndpoint")
 public class ApplicationStart {
     //Irá startar o servlet container que estiver utilizando / e o tomcat
     public static void main(String[] args) {
