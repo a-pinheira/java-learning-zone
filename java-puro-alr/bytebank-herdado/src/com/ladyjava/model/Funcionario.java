059bad6340c02620dc1be3a8bf5d6e2add6dde04
Mony Machado <monymachad@gmail.com>
2021-04-27 16:34:28 -0300

    Update project herança e interfaces - Alura

---
+++ java-puro-alr/bytebank-herdado/src/com/ladyjava/model/Funcionario.java
@@ -0,0 +1,45 @@
+package com.ladyjava.model;
+
+public class Funcionario {
+	private String nome;
+	private String cpf;
+	private double salario;
+
+	// Metodo de adcional de 10% no salario do funcionario
+	public double getBonificacai() {
+		return this.salario * 0.1;
+
+	}
+
+	// Constru tor
+	 /*public Funcionario(String nome, String cpf, double salario) {
+		//super();
+	
+	}*/
+
+	// Get's & Set's
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+}
---
+++ java-puro-alr/bytebank-herdado/src/com/ladyjava/view/TesteFuncionario.java
@@ -0,0 +1,23 @@
+package com.ladyjava.view;
+
+import com.ladyjava.model.Funcionario;
+
+public class TesteFuncionario {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Primeira forma: passando cada paramentros
+		Funcionario mony = new Funcionario();
+		mony.setNome("Simonetti");
+		mony.setCpf("22200112300");
+		mony.setSalario(2800.00);
+		
+		System.out.println("Nome da(o) funcionaria(o): " + mony.getNome());
+		System.out.println("Sua bonificação: " + mony.getBonificacai());
+		
+		//Segunda forma: deixando o usuário passar os dados (paramentros)
+		Funcionario func = new Funcionario();
+		
+
+	}
+
+}
