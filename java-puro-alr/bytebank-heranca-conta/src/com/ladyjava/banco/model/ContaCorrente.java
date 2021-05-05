4d1f9ef8d7e051db8b3098349bbdb6a8e85f70d9
Mony Machado <monymachad@gmail.com>
2021-05-05 00:21:22 -0300

    commit initial Conta - Alr

---
+++ java-puro-alr/bytebank-heranca-conta/src/com/ladyjava/banco/model/ContaCorrente.java
@@ -0,0 +1,11 @@
+package com.ladyjava.banco.model;
+
+//new ContaCorrente()
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero); //o mesmo que conta(), mas por converção do java é: super()
+		// TODO Auto-generated constructor stub
+	}
+
+}
