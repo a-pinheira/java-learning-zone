e507a1d165d79a416566bd970f27f2e76c6cfb77
Mony Machado <monymachad@gmail.com>
2021-05-27 12:08:35 -0300

    Java 8 - Pilhas - Alr

--- java-puro-alr/java-pilha/src/com/alr/ladyjava/controller/Fluxo.java
+++ java-puro-alr/java-pilha/src/com/alr/ladyjava/controller/Fluxo.java
@@ -4,21 +4,24 @@ public class Fluxo {
 
 	public static void main(String[] args) {
 		System.out.println("Inicio do main");
+		//chamada do metódo na main
 		metodo1();
 		System.out.println("Fim do main");
 	}
 
+	//chamada do metódo 1
 	private static void metodo1() {
 		System.out.println("Inicio do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	} 
 
+	//chamada do metódo 2
 	private static void metodo2() {
 		System.out.println("Inicio do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 		}
-		System.out.println("Fim do metodo2");
+		System.out.println("Fim do metodo2 - yep");
 	}
 }
