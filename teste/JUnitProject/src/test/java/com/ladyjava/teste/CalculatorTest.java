f4558b171c7935d335568631180e3b3f8120ae00
Mony Machado <monymachad@gmail.com>
2021-09-02 22:49:03 -0300

    Add TesteJUnit: Assets e validações iniciais - primeiro commit

---
+++ teste/JUnitProject/src/test/java/com/ladyjava/teste/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.ladyjava.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testSomar() {
+		Calculator calc = new Calculator();
+		//vamos testar o valor que o metodo somar retorna, no caso é o valor de INT (Inteiro)
+		int soma = calc.somar("1+3+1");
+		assertEquals(5, soma);
+
+	}
+
+}
