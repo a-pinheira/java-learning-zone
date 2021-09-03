202c21d46a6f9c682ae8fa747b26c0368bee7097
Mony Machado <monymachad@gmail.com>
2021-09-03 02:02:44 -0300

    Up unitary Test with Try Catch()

--- teste/JUnitProject/src/test/java/com/ladyjava/teste/ExceptionTest.java
+++ teste/JUnitProject/src/test/java/com/ladyjava/teste/ExceptionTest.java
@@ -1,5 +1,9 @@
 package com.ladyjava.teste;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,29 +14,46 @@ import org.junit.rules.ExpectedException;
 public class ExceptionTest {
 	//Teste de expected com IndexOutOfBoundsException
 	//simulamos true, para qnd é esperado que dê a exceção
+	
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void empty() {
 		//array list vazio e pega o primeiro objeto que não retorná nada
 		new ArrayList<Object>().get(0);
 	
 	//simulando falha uma situação em que não tenha a exceção com Lista de String
-	/*
-	 * List<String> lista = new ArrayList<String>();
-	 * lista.add("Simone");
-	 * lista.get(0);
-	 */
-	}
-	//Teste de expected com throws 
 	
+	   List<String> lista = new ArrayList<String>();
+	   lista.add("Simone");
+	   lista.get(0);
+	 
+	}
+	
+
+	//Teste de expected com throws	
 	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+	public ExpectedException thrown = ExpectedException.none(); 
 	
 	@Test
 	public void shouldTestExpectedExceptionMessage() throws IndexOutOfBoundsException {
 		List<Object> list = new ArrayList<Object>();
+		//esse teste serve somente para esta msg de string, ou seja, 
+		//precisa saber/conhecer qual a mensagen
+		thrown.expect(IndexOutOfBoundsException.class);
 		thrown.expectMessage("Index: 0, Size: 0");
 		list.get(0);
-		
 	}
-
+		
+	//Teste Unitário com Try catch Idiom
+	@Test
+	public void testExceptionMessage() {
+		try {
+			new ArrayList<Object>().get(0);
+			fail("Esperado que IndexOutOfBoundsException seja lançada");
+		} catch (IndexOutOfBoundsException ex) {
+			assertThat(ex.getMessage(), is("Index: 0, Size: 0"));
+			
+		}
+	}
 }
+
+
