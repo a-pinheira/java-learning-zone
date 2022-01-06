66ce8d506e981d62efd65743e80a23242c8cda3b
Mony Machado <monymachad@gmail.com>
2022-01-06 17:19:43 -0300

    [PCPIO] create and add EntryPoint and Employee

--- maven-gestor-app/src/main/java/org/ladyjava/endpoint/EntryPoint.java
+++ maven-gestor-app/src/main/java/org/ladyjava/endpoint/EntryPoint.java
@@ -1,4 +1,24 @@
 package org.ladyjava.endpoint;
 
+import org.ladyjava.model.Employee;
+import org.ladyjava.model.person;
+
 public class EntryPoint {
+    private String mode;
+
+    public EntryPoint(String mode) {
+        this.mode = mode;
+    }
+
+    public void printMode() {
+        System.out.println("App esta rodando em " + this.mode + "mode. \n");
+    }
+
+    public static void main(String[] args) {
+        EntryPoint ep = new EntryPoint( mode: "DEV" );
+        ep.printMode();
+        Employee admin = new Employee(name: "admin", id: 0, role: "admin" );
+        System.out.println("Novo user admin criado para o EntryPoint!");
+
+    }
 }
--- maven-gestor-app/src/main/java/org/ladyjava/model/Employee.java
+++ maven-gestor-app/src/main/java/org/ladyjava/model/Employee.java
@@ -1,12 +1,12 @@
 package org.ladyjava.model;
 
-public class Employee extends Person{
+public class Employee extends Person {
+
 
     private String role;
 
-    public Employee (String nome, long id, String role){
+    public Employee(String name, long id, String role) {
         super(name, id);
         this.role = role;
-
     }
 }
