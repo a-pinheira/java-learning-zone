afcf2a238f48fcb4b279c12601dd16ff4f738143
Mony Machado <monymachad@gmail.com>
2022-04-11 23:29:44 -0300

    [FORMULAS] add fibonacci

---
+++ formulas/fibonacci/src/com/ladyjava/formulas/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.ladyjava.formulas;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numeroA = 0;
+		int numeroB = 1;
+		int n = 12; //coloquei esta VAR N só poder mostrar a quantidade da sequencia
+		for (int i = 0; i < n; i++) {
+			System.out.println("Sequencia de " + n + " algoritmo, o resultato é " + numeroA);
+			numeroB = numeroA + numeroB;
+			numeroA = numeroB-numeroA;
+		}
+	}
+}
