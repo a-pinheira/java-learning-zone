e18adc234490a271f420bd20f5fe6705ac05777b
Machado <simone.p.machado@accenture.com>
2023-03-22 11:27:06 -0300

    [rest-api alr] update Repository

--- forum/src/main/java/br/com/alura/forum/repository/TopicosRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicosRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
 
-public interface TopicoRepository extends JpaRepository<Topico, Long> {
+public interface TopicosRepository extends JpaRepository<Topico, Long> {
 
 }
